34acdf7c30fd79dd4d638cc4970c8346be2df2cb
Viktoras Bezaras <v.bezaras@sportspar.de>
2020-06-09 09:39:31 +0200

    Removed using laravelcollective library, added autosubmit on change

--- resources/views/default/components/records_per_page.php
+++ resources/views/default/components/records_per_page.php
@@ -1,13 +1,14 @@
-<span>Records per page</span>
-<?php
-/** @var SportSpar\Grids\Components\RecordsPerPage $component */
-echo \Form::select(
-    $component->getInputName(),
-    $component->getVariants(),
-    $component->getValue(),
-    [
-        'class' => "form-control input-sm grids-control-records-per-page",
-        'style' => 'display: inline; width: 80px; margin-right: 10px'
-    ]
-);
-?>
+<?php /** @var SportSpar\Grids\Components\RecordsPerPage $component */ ?>
+<label style="font-weight: normal">
+    Records per page
+    <select class="form-control input-sm grids-control-records-per-page"
+            style="display: inline; width: 80px; margin-right: 10px"
+            onchange="submit()"
+            name="<?php echo $component->getInputName() ?>">
+        <?php foreach ($component->getVariants() as $key => $value): ?>
+            <option value="<?php echo $key ?>" <?php echo $component->getValue() === $value ? 'selected="selected"' : '' ?>>
+                <?php echo $value ?>
+            </option>
+        <?php endforeach; ?>
+    </select>
+</label>
