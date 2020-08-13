7f9f2c5866522ca45604ec1fe9b63b79910ff0df
mkuhnert-sportspar <66621607+mkuhnert-sportspar@users.noreply.github.com>
2020-08-13 13:34:02 +0200

    add StripTagsFormatter (#17)
    - add StripTagsFormatter
    - add test for StripTagsFormatter

---
+++ src/Formatter/StripTagsFormatter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SportSpar\Grids\Formatter;
+
+class StripTagsFormatter implements FormatterInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function format($value)
+    {
+        return strip_tags($value);
+    }
+}
