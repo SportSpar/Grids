c69b2abc136c335a19399bb216c1295a2b437d3f
Knut Kohl <k.kohl@sportspar.de>
2021-08-09 13:55:55 +0200

    Add table.grid and .grid-sorter

--- resources/views/default/grid.php
+++ resources/views/default/grid.php
@@ -1,18 +1,22 @@
 <form>
-<?php
-/** @var SportSpar\Grids\DataProvider\AbstractDataProvider $data **/
-/** @var SportSpar\Grids\Grid $grid **/
-?>
-<table class="table table-striped" id="<?= $grid->getConfig()->getName() ?>">
-<?= $grid->header() ? $grid->header()->render() : '' ?>
-<?php # ========== TABLE BODY ========== ?>
-<tbody>
-<?php while($row = $data->getRow()): ?>
-    <?= $grid->getConfig()->getRowComponent()->setDataRow($row)->render() ?>
-<?php endwhile; ?>
-</tbody>
-<?= $grid->footer() ? $grid->footer()->render() : '' ?>
-</table>
-<?php # Hidden input for submitting form by pressing enter if there are no other submits ?>
-<input type="submit" style="display: none;" />
+    <?php
+    /** @var SportSpar\Grids\DataProvider\AbstractDataProvider $data **/
+    /** @var SportSpar\Grids\Grid $grid **/
+    ?>
+    <table class="table table-striped grid" id="<?= $grid->getConfig()->getName() ?>">
+        <?= $grid->header() ? $grid->header()->render() : '' ?>
+        <?php
+        # ========== TABLE BODY ==========
+        ?>
+        <tbody>
+            <?php while ($row = $data->getRow()) : ?>
+                <?= $grid->getConfig()->getRowComponent()->setDataRow($row)->render() ?>
+            <?php endwhile; ?>
+        </tbody>
+        <?= $grid->footer() ? $grid->footer()->render() : '' ?>
+    </table>
+    <?php
+    # Hidden input for submitting form by pressing enter if there are no other submits
+    ?>
+    <input type="submit" style="display: none;" />
 </form>
